2077096233c7d79eb769533244af4ff1fa4dbdf4
basmdev <baskhanov.mr@mail.ru>
2024-08-08 14:57:19 +0300

    First code

--- run.py
+++ run.py
@@ -2,16 +2,18 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from config import TOKEN
+
 from app.handlers import router
 
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-
-
 async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Bot is closed')
