d6db3de8a7230583bdfee5cbb4ae7a8685f4e359
basmdev <baskhanov.mr@mail.ru>
2024-08-15 16:05:25 +0300

    First code

--- app/handlers.py
+++ app/handlers.py
@@ -5,6 +5,7 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
+import app.database.requests as rq
 
 
 router = Router()
@@ -15,7 +16,8 @@ class Reg(StatesGroup):
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Привет!', reply_markup=kb.main)
+    await rq.set_user(message.from_user.id)
+    await message.answer('Добро пожаловать в бота!', reply_markup=kb.main)
 
 @router.message(Command('help'))
 async def cmd_help(message: Message):
--- run.py
+++ run.py
@@ -4,9 +4,11 @@ from aiogram import Bot, Dispatcher
 from config import TOKEN
 
 from app.handlers import router
+from app.database.models import async_main
 
 
 async def main():
+    await async_main()
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
